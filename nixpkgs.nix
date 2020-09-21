bd5917f0d132684bc82a8d5f8d85599cefd5a6fb
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2020-09-21 09:38:41 +0100

    Add nix-shell to project

---
+++ nixpkgs.nix
@@ -0,0 +1,5 @@
+builtins.fetchGit {
+  url = "https://github.com/NixOS/nixpkgs-channels";
+  rev = "773ef5ae78339db744b917c2b35d28812028bd8c";
+  ref = "nixos-20.03";
+}
---
+++ shell.nix
@@ -0,0 +1,25 @@
+{ nixpkgs ? (import ./nixpkgs.nix) }:
+let
+  pkgs = import nixpkgs {};
+
+in pkgs.stdenv.mkDerivation rec {
+  name = "esp8266-platformio-dev-shell";
+
+  src = builtins.toString ./.;
+
+  buildInputs = with pkgs; [
+    esptool
+    esptool-ck
+    platformio
+    gnumake
+  ];
+
+  HISTFILE = "${src}/.bash_hist";
+  # https://docs.platformio.org/en/latest/envvars.html#directories
+
+  PLATFORMIO_CORE_DIR = "${src}/.platformio";
+
+  shellHook = ''
+    echo "Ready!"
+  '';
+}
